cc16e2ce5c6162f38239dc5a8dd754660a003862
motionrus <tytin@narod.ru>
2018-06-01 17:08:08 +0300

    decorate

--- example2.py
+++ example2.py
@@ -122,3 +122,108 @@ decorate_func(print_name)('Иван', 'Иванов')
 # Передали Иван, Иванов
 # Имя: Иван, Фамилия: Иванов
 # Конец декорируемой функции
+
+# Передача аргументов в декорируемую функцию
+def decorate_multiple_args(func):
+    def wrapper_func(*args, **kwargs):
+        print("before call decorate func".upper())
+        print(args)
+        print(kwargs)
+        print("END")
+        func(*args, **kwargs)
+        print("after call decorate func".upper())
+        print("END")
+    return wrapper_func
+
+@decorate_multiple_args
+def print_args(*args, **kwargs):
+    print("positional args:".upper())
+    i = 0
+    for arg in args:
+        i += 1
+        print('{} lists arg = "{}"'.format(i, arg))
+    print("named kwargs:".upper())
+    i = 0
+    for key in kwargs:
+        i += 1
+        print('key "{}" have value "{}"'.format(key, kwargs[key]))
+
+print_args([1,2,3], {'a': 1, 'b': 2, 'c': 3}, 4, 5, d=4, e={'A':1,'B':2})
+
+# result
+# BEFORE CALL DECORATE FUNC
+# ([1, 2, 3], {'a': 1, 'b': 2, 'c': 3}, 4, 5)
+# {'d': 4, 'e': {'A': 1, 'B': 2}}
+# END
+# POSITIONAL ARGS:
+# 1 lists arg = "[1, 2, 3]"
+# 2 lists arg = "{'a': 1, 'b': 2, 'c': 3}"
+# 3 lists arg = "4"
+# 4 lists arg = "5"
+# NAMED KWARGS:
+# key "d" have value "4"
+# key "e" have value "{'A': 1, 'B': 2}"
+# AFTER CALL DECORATE FUNC
+# END
+
+
+# Расширение функционала декораторов, передача аргументов в декоратор и декорируемую функцию
+#
+arg1 = 'decorate'
+arg2 = 'decorate2'
+arg_func = 'arg_func'
+def decorate_multiple_args(*args):
+    print(args)
+    def decorating_func(func):
+        def wrapped(*args, **kwargs):
+            func(*args, **kwargs)
+        return wrapped
+    return decorating_func
+
+@decorate_multiple_args(arg1, arg2)
+def decorated_func(*args, **kwargs):
+    print(args, kwargs)
+
+decorated_func(arg_func)
+
+# result
+# ('decorate', 'decorate2')
+# ('arg_func',) {}
+
+# CHECK DIFFERENT TIMEOVER EXTEND LIST
+#
+
+FILE = '/home/rus/Загрузки/logic.memory.profiler.txt'
+def benchmark(func):
+    def wrapper(first):
+        import time
+        time_now = time.clock()
+        func(first)
+        print((time.clock() - time_now)*10**6)
+    return wrapper
+
+@benchmark
+def add_list(list_file):
+    list_file.extend([1])
+
+@benchmark
+def add_list2(list_file):
+    list_file += [1]
+
+@benchmark
+def add_list3(list_file):
+    list_file = list_file + [1]
+
+with open(FILE, 'r') as f:
+    list_file = f.readlines()
+
+print(len(list_file))
+add_list(list_file)
+add_list2(list_file)
+add_list3(list_file)
+
+# RESULT
+# 4018294
+# 5.000000001587068
+# 5.999999999062311
+# 157233.0000000015
